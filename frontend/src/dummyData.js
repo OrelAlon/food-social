2b370a65ce50ff215852dc85964caebc497fc14f
OrelAlon <orela231089@gmail.com>
2022-06-22 18:51:55 +0300

    dummy data

---
+++ frontend/src/dummyData.js
@@ -0,0 +1,83 @@
+export const Users = [
+  {
+    id: 2,
+    profilePicture:
+      "https://image.shutterstock.com/image-photo/surreal-concept-roll-world-dice-260nw-1356798002.jpg",
+    username: "gili alon",
+  },
+  {
+    id: 3,
+    profilePicture:
+      "https://aot-wpengine.netdna-ssl.com/wp-content/uploads/2014/04/animal_facts-e1396431549968.jpg",
+    username: "ortal levi",
+  },
+  {
+    id: 4,
+    profilePicture:
+      "https://sm.ign.com/t/ign_il/video/l/lost-in-ra/lost-in-random-official-launch-trailer_td77.280.jpg",
+    username: "lihi shtrit",
+  },
+  {
+    id: 5,
+    profilePicture: "https://pbs.twimg.com/media/D8Dp0c5WkAAkvME.jpg",
+    username: "tom zilberman",
+  },
+];
+
+export const Restaurants = [
+  {
+    id: 2,
+    profilePicture:
+      "https://bestrest.rest/wp-content/uploads/2019/03/1ARIA_FINAL_LOGO-%D7%A2%D7%95%D7%AA%D7%A7.jpg",
+    name: "ARIA",
+  },
+  {
+    id: 3,
+    profilePicture:
+      "https://res.cloudinary.com/ontopo/image/upload/c_crop,x_0,y_0,w_376,h_376/v1632298624/assets/VuHr8kl6Gse/poyE-rXSjD6/1.png",
+    name: "a",
+  },
+  {
+    id: 4,
+    profilePicture: "https://media.easy.co.il/images/PICS/4822824.jpg",
+    name: "Emesh",
+  },
+  {
+    id: 5,
+    profilePicture:
+      "https://www.ocdtlv.com/wp-content/uploads/2017/01/logo-main.png",
+    name: "OCD",
+  },
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: "Love For All, Hatred For None.",
+    photo:
+      "https://media.moddb.com/cache/images/members/5/4550/4549205/thumb_620x2000/duck.jpg",
+    date: "5 mins ago",
+    userId: 1,
+    like: 32,
+    comment: 9,
+  },
+
+  {
+    id: 4,
+    photo:
+      "https://images.unsplash.com/photo-1513542789411-b6a5d4f31634?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    date: "4 hours ago",
+    userId: 4,
+    like: 7,
+    comment: 3,
+  },
+  {
+    id: 5,
+    photo:
+      "https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    date: "5 hours ago",
+    userId: 5,
+    like: 23,
+    comment: 5,
+  },
+];
