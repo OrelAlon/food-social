d2c25b36bef20fd42ab3a1ddf960bc8f92bb92ae
OrelAlon <orela231089@gmail.com>
2022-06-22 18:20:14 +0300

    move env

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const helmet = require("helmet");
 const multer = require("multer");
 const path = require("path");
@@ -11,64 +11,62 @@ const port = process.env.PORT || 8000;
 // const morgan = require("morgan");
 
 // routes
-const userRoute = require("./routes/users");
-const postRoute = require("./routes/posts");
-const authRoute = require("./routes/auth");
-const restaurantRoute = require("./routes/restaurants");
-
-dotenv.config();
+// const userRoute = require("./routes/users");
+// const postRoute = require("./routes/posts");
+// const authRoute = require("./routes/auth");
+// const restaurantRoute = require("./routes/restaurants");
 
 mongoose.connect(process.env.MONGO_URL, () => {
   console.log("Connectes to MongoDB!!!!");
 });
 
-app.use("/images", express.static(path.join(__dirname, "public/images")));
+// app.use("/images", express.static(path.join(__dirname, "public/images")));
 
-// middleware
-app.use(express.json());
-app.use(helmet());
-// app.use(morgan("common"));
+// // middleware
+// app.use(express.json());
+// app.use(helmet());
+// // app.use(morgan("common"));
 
-// multer - upload files
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
+// // multer - upload files
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "public/images");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, req.body.name);
+//   },
+// });
 
-const upload = multer({ storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  try {
-    return res.status(200).json("file uploaded!!!!!");
-  } catch (error) {
-    console.log(":( :( OOps " + error);
-  }
-});
+// const upload = multer({ storage });
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//   try {
+//     return res.status(200).json("file uploaded!!!!!");
+//   } catch (error) {
+//     console.log(":( :( OOps " + error);
+//   }
+// });
 
-//routes
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/auth", authRoute);
-app.use("/api/restaurants", restaurantRoute);
+// //routes
+// app.use("/api/users", userRoute);
+// app.use("/api/posts", postRoute);
+// app.use("/api/auth", authRoute);
+// app.use("/api/restaurants", restaurantRoute);
 
-// Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+// // Serve frontend
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(
+//       path.resolve(__dirname, "../", "frontend", "build", "index.html")
+//     )
+//   );
+// } else {
+//   app.get("/", (req, res) => res.send("Please set to production"));
+// }
 
 app.listen(port, () => {
   console.log("connecteddddd");
 });
 
-//  ==> npm run dev
+// //  ==> npm run dev
